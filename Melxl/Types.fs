af5ee852dd58a30535f975d5687884dcf75394eb
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-03-18 17:27:31 +0700

    types module and some types added.

---
+++ Melxl/Types.fs
@@ -0,0 +1,14 @@
+﻿module Melxl.Types
+
+type KindOfNonBinary =
+    | Agender
+    | Demigender
+    | Genderfluid
+    | Xenogender
+    | Pangender
+    | Other
+
+type Gender =
+    | Male
+    | Female
+    | NonBinary of KindOfNonBinary
