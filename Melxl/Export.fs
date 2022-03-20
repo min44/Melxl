ca7d123c23a1a7e267ec41ce6352085446b000e0
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-03-21 01:45:30 +0700

    Excel autorun added.

--- Melxl/Export.fs
+++ Melxl/Export.fs
@@ -10,5 +10,7 @@ let ExportToExcel data =
     let fileName = Path.ChangeExtension($"melxl_export_{id}", ".xlsx")
     let dstPath = Path.Combine(desktop, fileName)
     let sheet = Sheet()
+    sheet.ColumnsWidth <- [| 60.0 |]
     use ev = new ExcelWriter(dstPath, sheet)
     data |> Seq.iteri(fun i x -> ev.Write(x, 1, i + 1))
+    dstPath
--- Melxl/Program.fs
+++ Melxl/Program.fs
@@ -4,10 +4,17 @@ open Melxl
 open Melxl.Domain
 open Provider
 open Export
+open System.Diagnostics
 
 let personsGreetings =
     Info
     |> Seq.map Person
     |> Seq.map(fun x -> x.GreetingsText)
 
-ExportToExcel personsGreetings
+let file = ExportToExcel personsGreetings
+let myProcess = new Process()
+myProcess.StartInfo.UseShellExecute <- true
+myProcess.StartInfo.FileName <- file
+myProcess.StartInfo.WindowStyle <- ProcessWindowStyle.Maximized
+myProcess.Start() |> ignore
+myProcess.WaitForExit()
